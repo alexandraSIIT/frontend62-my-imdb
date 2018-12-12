153763100c74940b300ef70b31d558a75ba44c37
Joey06082018 <ovidiujecan@yahoo.com>
2018-12-12 20:22:56 +0200

    update

--- views/home.js
+++ views/home.js
@@ -3,60 +3,44 @@ var movies = new Movies();
 getMovies();
 function getMovies() {
   movies.getMovies().then(function() {
-    console.log("List movies", movies.items);
     displayMovies(movies.items);
-  });
-}
-
+    });
+    }
   var template = document.getElementById("template");
-  
-function displayMovies(response) {
-    
-var template = document.getElementById("template");
-  // traverse the array (parcurgerea sirului)
-  for (var i = 0; i < 10; i++) {
-    console.log(response[i])
+  function displayMovies(response) {
+    var template = document.getElementById("template");
+    for (var i = 0; i < 10; i++) {
     var moviesContainer = document.getElementById("movies");
-    // clone the template; copy template's children as well (deep parameter = true)
     var movieClone = template.cloneNode(true);
-    // set a unique id for each article
-    
     movieClone.id = "movie_" + response[i].id;
-    // populate the cloned template
-    
     var movieTitleElement = movieClone.querySelector(".movie-title");
     movieTitleElement.innerHTML = response[i].title;
     var imageUrl = movieClone.querySelector(".myImage");
     imageUrl.setAttribute("src", response[i].poster);
     var movieRatingElement = movieClone.querySelector(".rating");
     movieRatingElement.innerHTML = response[i].rating;
-var movieGenreElement = movieClone.querySelector(".genre");
+    var movieGenreElement = movieClone.querySelector(".genre");
     movieGenreElement.innerHTML = response[i].genre;
-var movieYearElement = movieClone.querySelector(".year");
+    var movieYearElement = movieClone.querySelector(".year");
     movieYearElement.innerHTML = response[i].year;
 
 
-  //   var deleteButton = gameClone.querySelector(".game-delete");
+  //   var deleteButton = movieClone.querySelector(".movie-delete");
   //   deleteButton.addEventListener("click", function(event) {
   //     console.log("event", event);
-  //     // event.target = the button that we clicked
-  //     // the clicked button has a div as parent, and that has the article as parent
-  //     var grandpa = event.target.parentNode.parentNode; // the article
-  //     var grandpaId = grandpa.id; // article_3
-  //     var gameId = grandpaId.replace("game_", ""); // 3
-  //     // call delete article request from model
-  //     var game = new Game({ id: gameId });
-  //     game.deleteGame().then(function() {
-  //       removeGameFromDOM(response, grandpa);
+  //     var grandpa = event.target.parentNode.parentNode; 
+  //     var grandpaId = grandpa.id; 
+  //     var movieId = grandpaId.replace("movie_", ""); 
+  //     var movie = new Movie({ id: movieId });
+  //     movie.deleteMovie().then(function() {
+  //       removeMovieFromDOM(response, grandpa);
   //     });
   //   });
 
-  //   var editButton = gameClone.querySelector(".game-edit");
-  //   editButton.addEventListener("click", updateGameOnClick);
+  //   var editButton = movieClone.querySelector(".movie-edit");
+  //   editButton.addEventListener("click", updateMovieOnClick);
 
    moviesContainer.appendChild(movieClone);
   }
-
-  // remove the template from DOM
   //template.remove();
 }
