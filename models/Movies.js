befffef10146203611be40dd90dffc5654bcf18f
Roxana Ilies <roxanna.lorinczi@gmail.com>
2018-12-14 19:58:46 +0200

    implement search movie

--- models/Movies.js
+++ models/Movies.js
@@ -1,5 +1,6 @@
 function Movies() {
     this.items = [];
+    this.moviesFiltered = [];
 }
 
 var movieUrl = "https://ancient-caverns-16784.herokuapp.com/movies";
@@ -11,8 +12,20 @@ Movies.prototype.getMovies = function() {
 	for (var i = 0; i < response.results.length; i++) {
       var movie = new Movie(response.results[i]);
       me.items.push(movie);
-      console.log(movie);
     }
 
     });
 };
+
+Movies.prototype.getMoviesFilter = function(category, searchParam) {
+    var movieParamsUrl = movieUrl + '?' + category + '=' + searchParam;
+    var me = this;
+    return $.get(movieParamsUrl).then(function(response){
+        console.log(response);
+        for (var i = 0; i < response.results.length; i++) {
+            var mov = new Movie(response.results[i]);
+            me.moviesFiltered.push(mov);
+            console.log(mov);
+          }
+    })
+}
--- views/home.js
+++ views/home.js
@@ -2,61 +2,102 @@ var movies = new Movies();
 
 getMovies();
 function getMovies() {
-  movies.getMovies().then(function() {
-    console.log("List movies", movies.items);
+  movies.getMovies().then(function () {
+    // console.log("List movies", movies.items);
     displayMovies(movies.items);
   });
 }
 
-  var template = document.getElementById("template");
-  
-function displayMovies(response) {
-    
 var template = document.getElementById("template");
+function displayMovies(response) {
+  console.log(response)
+  var moviesContainer = document.getElementById("movies");
+  // var template = document.getElementById("template");
   // traverse the array (parcurgerea sirului)
-  for (var i = 0; i < 10; i++) {
-    console.log(response[i])
-    var moviesContainer = document.getElementById("movies");
+  for (var i = 0; i < response.length; i++) {
+
     // clone the template; copy template's children as well (deep parameter = true)
     var movieClone = template.cloneNode(true);
     // set a unique id for each article
-    
+
     movieClone.id = "movie_" + response[i].id;
+    movieClone.classList.add('mov');
     // populate the cloned template
-    
+
     var movieTitleElement = movieClone.querySelector(".movie-title");
     movieTitleElement.innerHTML = response[i].title;
     var imageUrl = movieClone.querySelector(".myImage");
-    imageUrl.setAttribute("src", response[i].poster);
+    if (response[i].poster == 'N/A') {
+      imageUrl.setAttribute("src", '../movie-default-image.jpg');
+    } else {
+      imageUrl.setAttribute("src", response[i].poster);
+    }
     var movieRatingElement = movieClone.querySelector(".rating");
     movieRatingElement.innerHTML = response[i].rating;
-var movieGenreElement = movieClone.querySelector(".genre");
-    movieGenreElement.innerHTML = response[i].genre;
-var movieYearElement = movieClone.querySelector(".year");
-    movieYearElement.innerHTML = response[i].year;
+    // var movieGenreElement = movieClone.querySelector(".genre");
+    // movieGenreElement.innerHTML = response[i].genre;
+    // var movieYearElement = movieClone.querySelector(".year");
+    // movieYearElement.innerHTML = response[i].year;
 
 
-  //   var deleteButton = gameClone.querySelector(".game-delete");
-  //   deleteButton.addEventListener("click", function(event) {
-  //     console.log("event", event);
-  //     // event.target = the button that we clicked
-  //     // the clicked button has a div as parent, and that has the article as parent
-  //     var grandpa = event.target.parentNode.parentNode; // the article
-  //     var grandpaId = grandpa.id; // article_3
-  //     var gameId = grandpaId.replace("game_", ""); // 3
-  //     // call delete article request from model
-  //     var game = new Game({ id: gameId });
-  //     game.deleteGame().then(function() {
-  //       removeGameFromDOM(response, grandpa);
-  //     });
-  //   });
+    //   var deleteButton = gameClone.querySelector(".game-delete");
+    //   deleteButton.addEventListener("click", function(event) {
+    //     console.log("event", event);
+    //     // event.target = the button that we clicked
+    //     // the clicked button has a div as parent, and that has the article as parent
+    //     var grandpa = event.target.parentNode.parentNode; // the article
+    //     var grandpaId = grandpa.id; // article_3
+    //     var gameId = grandpaId.replace("game_", ""); // 3
+    //     // call delete article request from model
+    //     var game = new Game({ id: gameId });
+    //     game.deleteGame().then(function() {
+    //       removeGameFromDOM(response, grandpa);
+    //     });
+    //   });
 
-  //   var editButton = gameClone.querySelector(".game-edit");
-  //   editButton.addEventListener("click", updateGameOnClick);
+    //   var editButton = gameClone.querySelector(".game-edit");
+    //   editButton.addEventListener("click", updateGameOnClick);
 
-   moviesContainer.appendChild(movieClone);
+    moviesContainer.appendChild(movieClone);
   }
 
   // remove the template from DOM
-  //template.remove();
+  template.remove();
 }
+
+var categoryDropDown = document.getElementById('search-category');
+var searchInput = document.getElementById('movie-search');
+var searchBtn = document.getElementById('search-button');
+searchBtn.addEventListener("click", searchMovie);
+
+function searchMovie() {
+  var selectedCategory = categoryDropDown.value;
+  var searchInputValue = searchInput.value;
+  if (searchInputValue) {
+    removeExistentMovies();
+    movies.getMoviesFilter(selectedCategory, searchInputValue);
+    console.log(movies.moviesFiltered)
+    displayMovies(movies.moviesFiltered);
+  } else {
+    searchInput.style.border = '2px solid red';
+  }
+}
+
+function removeExistentMovies() {
+  var movieDiv = document.getElementsByClassName('mov');
+  while (movieDiv[0]) {
+    movieDiv[0].parentNode.removeChild(movieDiv[0]);
+  }
+}
+
+categoryDropDown.addEventListener('change', function () {
+  searchInput.style.border = 'none';
+  searchInput.value = '';
+  removeExistentMovies();
+  displayMovies(movies.items);
+})
+
+searchInput.addEventListener('keyup', function(){
+  removeExistentMovies();
+  displayMovies(movies.items);
+})
