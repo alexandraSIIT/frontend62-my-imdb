c40c75cc0f4c434f9f18f0d1f313dcdd960ed93a
boti2000 <boti.kiss@yahoo.com>
2018-12-17 21:06:33 +0200

    pray

--- models/Auth.js
+++ models/Auth.js
@@ -2,19 +2,11 @@ var url = "https://ancient-caverns-16784.herokuapp.com/"
 function Register(dataString) {
     $.ajax({
         type: "POST",
-<<<<<<< HEAD
         url: url + "auth/register",
         data: dataString,
         success: function(response) {
             console.log(response)
             localStorage.setItem("AccesToken", response.accessToken)
-=======
-        url: "https://ancient-caverns-16784.herokuapp.com/auth/register", // TODO: move the base url somewhere in a variable and reuse it
-        data: dataString,
-        success: function(response) {
-            console.log(response)
-            localStorage.setItem("Acces Token", response.accessToken) // TODO: use local storage keys without spaces
->>>>>>> 472e842b377c705890944b59e4ae36c24a474dc7
         },
         error: function() {
             $("h3#taken-username").show();
@@ -30,7 +22,6 @@ function Login(dataString2) {
         data: dataString2,
         success: function(response) {
             console.log(response)
-<<<<<<< HEAD
             localStorage.setItem("AccesToken", response.accessToken)
         }
     })
@@ -45,9 +36,6 @@ function Logout(token) {
         },
         success: function(response) {
             console.log(response);
-=======
-            localStorage.setItem("Acces Token2", response.accessToken) // TODO: use the same key as for registration
->>>>>>> 472e842b377c705890944b59e4ae36c24a474dc7
         }
     })
 }
